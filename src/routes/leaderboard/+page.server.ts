e412816fc42e4c5ec3f31a7eac722db7524036b5
Neeraj <neerajkohli472@gmail.com>
2026-02-10 17:17:13 +0530

    update firestore rules and leaderboard

--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -8,23 +8,25 @@ export const load = async ({ params, locals }: any) => {
     if (!loaded) {
         const qSnap = await queryDef.get();
         qSnap.docs.forEach((e) => {
-            const data = e.data();
+            const data = e.data() || {};
+            const members = data.members || [];
             leaderboard.push({
-                teamName: data.teamName,
-                score: (data.level - 1) * 100,
-                members: data.members.length,
-                gsv: data.gsv_verified
+                teamName: data.teamName || "Unknown Team",
+                score: ((data.level || 1) - 1) * 100,
+                members: members.length,
+                gsv: data.gsv_verified || false
             });
         });
         queryDef.onSnapshot((snap) => {
             const newData: any[] = [];
             snap.docs.forEach((e) => {
-                const data = e.data()
+                const data = e.data() || {};
+                const members = data.members || [];
                 newData.push({
-                    teamName: data.teamName,
-                    score: (data.level - 1) * 100,
-                    members: data.members.length,
-                    gsv: data.gsv_verified
+                    teamName: data.teamName || "Unknown Team",
+                    score: ((data.level || 1) - 1) * 100,
+                    members: members.length,
+                    gsv: data.gsv_verified || false
                 });
             });
             leaderboard = newData;
